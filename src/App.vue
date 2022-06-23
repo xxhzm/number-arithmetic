e0d8564d6b1d681e4a5bc5c08c47d08142ad6f8b
小小孩子们 <2595557847@qq.com>
2022-06-23 20:45:45 +0800

    feat: 增加有理数加法、有理数减法、有理数乘法、有理数除法。

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--
+    0: 有理数加法
+    1: 有理数减法
+    2: 有理数乘法
+    3: 有理数除法
+    4: 有理数加减乘除混合
+  -->
+  <div class="form">
+    <el-form :model="form" :inline="true">
+      <el-form-item label="id">
+        <el-select v-model="form.id" placeholder="id">
+          <el-option label="有理数加法" value="0" />
+          <el-option label="有理数减法" value="1" />
+          <el-option label="有理数乘法" value="2" />
+          <el-option label="有理数除法" value="3" />
+          <el-option label="有理数加减乘除混合" value="4" />
+        </el-select>
+      </el-form-item>
+
+      <el-input v-model="form.maxNumber" placeholder="最大值" />
+      <el-input v-model="form.number" placeholder="数量" />
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Create</el-button>
+        <el-button @click="onClear">clear</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+
+  <p v-for="item in result" :key="item">{{ item }}</p>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { reactive, ref } from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+// do not use same name with ref
+const form = reactive({
+  id: '0',
+  maxNumber: 100,
+  number: ''
+})
+
+const result = ref([])
+
+const onSubmit = () => {
+  for (let index = 0; index < form.number; index++) {
+    let x = Math.floor(Math.random() * form.maxNumber) + 1
+    let y = Math.floor(Math.random() * form.maxNumber) + 1
+
+    const upper = Math.floor(Math.random() * 2)
+    const under = Math.floor(Math.random() * 2)
+
+    if (upper === 0) {
+      x = '(-' + x + ')'
+    }
+
+    if (under === 0) {
+      y = '(-' + y + ')'
+    }
+
+    const arr = ['+', '-', '×', '÷']
+
+    // if (form.id === '0') {
+    //   temp = x + '+' + y
+    // } else if (form.id === '1') {
+    //   temp = x + '-' + y
+    // } else if (form.id === '2') {
+    //   temp = x + '*' + y
+    // } else if (form.id === '3') {
+    //   temp = x + '/' + y
+    // } else if (form.id === '4') {
+    //   temp = x + '-' + y
+    // }
+
+    result.value.push(x + arr[form.id] + y)
   }
+}
+
+const onClear = () => {
+  result.value = []
 }
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="less" scoped>
+.form {
+  width: 30vw;
+  margin: 0 auto;
+}
+
+p {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+@media screen and (max-width: 1200px) {
+  .form {
+    width: 70vw !important;
+  }
 }
 </style>
